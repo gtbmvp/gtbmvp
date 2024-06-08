641af4f2b76dba175e91235a1672b80bd1beaa55
gtb <grindingeorge@gmail.com>
2024-06-08 20:28:53 +0300

    fix: update eslint_d config

--- src/app/Localization/i18n.ts
+++ src/app/Localization/i18n.ts
@@ -1,4 +1,4 @@
-import i18n, { InitOptions } from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // the translations
@@ -36,6 +36,6 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
         interpolation: {
             escapeValue: false, // react already safes from xss
         },
-    } as InitOptions);
+    });
 
 export default i18n;
